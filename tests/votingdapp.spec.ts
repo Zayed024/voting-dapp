2456e6e7fd2dd3aa837b34e832895f003960d0dc
Zayed <amanzayed90@gmail.com>
2025-01-15 05:21:10 +0000

    Updated code for voting-dapp with latest changes

---
+++ tests/votingdapp.spec.ts
@@ -0,0 +1,96 @@
+//import { PartialAccounts } from './../node_modules/@coral-xyz/anchor/dist/cjs/program/namespace/methods.d';
+import * as anchor from '@coral-xyz/anchor';
+import { Program } from '@coral-xyz/anchor';
+import { Keypair, PublicKey, SystemProgram } from '@solana/web3.js';
+import { Voting } from '../target/types/voting';
+import { startAnchor } from "solana-bankrun";
+import { BankrunProvider } from "anchor-bankrun";
+
+const IDL = require('../target/idl/voting.json');
+
+const votingAddress = new PublicKey("6uaHTsTcLBMnDSy5z6eqZpPbUspMvoeA1fhCxLnCTMnu");
+
+describe('Voting', () => {
+
+  let context;
+  let provider;
+  //let votingProgram;
+  anchor.setProvider(anchor.AnchorProvider.env());
+  let votingProgram = anchor.workspace.Voting as Program<Voting>;
+
+  beforeAll(async () => {
+    context = await startAnchor("", [{name: "votingdapp", programId: votingAddress}], []);
+    provider = new BankrunProvider(context);
+
+     votingProgram = new Program<Voting>(
+      IDL,
+      provider,
+    );
+  })
+
+  it('Initialize Poll', async () => {
+    await votingProgram.methods.initializePoll(
+      new anchor.BN(1),
+      "Who is your preffered candidate?",
+      new anchor.BN(0),
+      new anchor.BN(1821246480),
+    ).rpc();
+
+    const [pollAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, 'le', 8)],
+      votingAddress,
+    )
+
+    const poll = await votingProgram.account.poll.fetch(pollAddress);
+
+    console.log(poll);
+
+    expect(poll.pollId.toNumber()).toEqual(1);
+    expect(poll.description).toEqual("Who is your preffered candidate?");
+    expect(poll.pollStart.toNumber()).toBeLessThan(poll.pollEnd.toNumber());
+  });
+
+  it("initialize candidate", async() => {
+    await votingProgram.methods.initializeCandidate(
+      "Trump",
+      new anchor.BN(1),
+    ).rpc();
+    await votingProgram.methods.initializeCandidate(
+      "Biden",
+      new anchor.BN(1),
+    ).rpc();
+
+    const [bidenAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, 'le', 8), Buffer.from("Biden")],
+      votingAddress,
+    );
+    const bidenCandidate = await votingProgram.account.candidate.fetch(bidenAddress);
+    console.log(bidenCandidate);
+    expect(bidenCandidate.candidateVotes.toNumber()).toEqual(0);
+
+    const [trumpAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, 'le', 8), Buffer.from("Trump")],
+      votingAddress,
+    );
+    const trumpCandidate = await votingProgram.account.candidate.fetch(trumpAddress);
+    console.log(trumpCandidate);
+    expect(trumpCandidate.candidateVotes.toNumber()).toEqual(0);
+  });
+
+  it("vote", async() => {
+    await votingProgram.methods
+      .vote(
+        "Trump",
+        new anchor.BN(1)
+      ).rpc()
+
+    const [trumpAddress] = PublicKey.findProgramAddressSync(
+      [new anchor.BN(1).toArrayLike(Buffer, 'le', 8), Buffer.from("Trump")],
+      votingAddress,
+    );
+    const trumpCandidate = await votingProgram.account.candidate.fetch(trumpAddress);
+    console.log(trumpCandidate);
+    expect(trumpCandidate.candidateVotes.toNumber()).toEqual(1);
+  });
+
+});
