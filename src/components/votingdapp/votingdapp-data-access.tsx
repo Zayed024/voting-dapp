e4ccd0da17da52265638d13931ac8e7fafe52695
Zayed024 <143921101+Zayed024@users.noreply.github.com>
2025-01-17 20:13:49 +0530

    Update votingdapp-data-access.tsx

--- src/components/votingdapp/votingdapp-data-access.tsx
+++ src/components/votingdapp/votingdapp-data-access.tsx
@@ -52,7 +52,7 @@ export function useVotingdappProgram() {
         new BN(pollEnd) // Convert to BN
       )
       .accounts({
-        poll: pollPDA, // Use the derived PDA
+        Poll: pollPDA, // Use the derived PDA
         signer: provider.wallet.publicKey, // Wallet signer
         systemProgram: PublicKey.default, // System program
       })
