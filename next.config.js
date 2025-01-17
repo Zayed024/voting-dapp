7a9df6c3d5e71f0b0a88640d0e6c7516dfb9cf47
Zayed024 <143921101+Zayed024@users.noreply.github.com>
2025-01-17 12:24:36 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
-//@ts-check
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { composePlugins, withNx } = require('@nx/next');
-
 /**
- * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
+ * @type {import('next').NextConfig}
  **/
 const nextConfig = {
   webpack: (config) => {
@@ -15,16 +10,6 @@ const nextConfig = {
     ];
     return config;
   },
-  nx: {
-    // Set this to true if you would like to use SVGR
-    // See: https://github.com/gregberge/svgr
-    svgr: false,
-  },
 };
 
-const plugins = [
-  // Add more Next.js plugins to this list if needed.
-  withNx,
-];
-
-module.exports = composePlugins(...plugins)(nextConfig);
+module.exports = nextConfig;
